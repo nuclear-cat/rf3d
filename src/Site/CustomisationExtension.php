04e209713a5193016b502592ce047974dcc68044
Ilya Shibailo <ilya.sh@zimalab.com>
2019-11-26 21:43:21 +0700

    fix

--- src/Site/CustomisationExtension.php
+++ src/Site/CustomisationExtension.php
@@ -17,4 +17,36 @@ use Bolt\Extension\SimpleExtension;
  */
 class CustomisationExtension extends SimpleExtension
 {
+    //https://stackoverflow.com/questions/52754936/overwrite-backend-template-in-bolt-cms
+    protected function registerTwigPaths()
+    {
+        if ($this->getEnd() == 'backend') {
+            return [
+                'view' => ['position' => 'prepend', 'namespace' => 'bolt']
+            ];
+        }
+        return [];
+    }
+
+    private function getEnd()
+    {
+        $backendPrefix = $this->container['config']->get('general/branding/path');
+        $end = $this->container['config']->getWhichEnd();
+
+        switch ($end) {
+            case 'backend':
+                return 'backend';
+            case 'async':
+                // we have async request
+                // if the request begin with "/admin" (general/branding/path)
+                // it has been made on backend else somewhere else
+                $url = '/' . ltrim($_SERVER['REQUEST_URI'], $this->container['paths']['root']);
+                $adminUrl = '/' . trim($backendPrefix, '/');
+                if (strpos($url, $adminUrl) === 0) {
+                    return 'backend';
+                }
+            default:
+                return $end;
+        }
+    }
 }
