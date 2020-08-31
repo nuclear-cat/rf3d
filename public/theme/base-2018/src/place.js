e31e3164062fe7a9eff10fa326a9c23048243bda
Ilya Shibailo <ilya.shibailo@gmail.com>
2020-09-01 04:03:05 +0700

    commit

--- public/theme/base-2018/src/place.js
+++ public/theme/base-2018/src/place.js
@@ -5,5 +5,29 @@ $('.js-rooms-slider').slick({
     dots: false,
     // centerMode: true,
     focusOnSelect: true,
-    lazyLoad: 'progressive'
+    lazyLoad: 'progressive',
+    responsive: [
+    {
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+        }
+    },
+    {
+        breakpoint: 600,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+        }
+    },
+    {
+        breakpoint: 480,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }
+    }]
 });
