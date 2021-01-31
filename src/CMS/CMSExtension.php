525c4ef68a4c32f4c9d421d137ccbd1d31fc1acc
Ilya Shibailo <ilya.shibailo@gmail.com>
2021-02-01 02:43:54 +0700

    commit

--- src/CMS/CMSExtension.php
+++ src/CMS/CMSExtension.php
@@ -4,13 +4,39 @@ namespace Bundle\CMS;
 
 use Bolt\Extension\SimpleExtension;
 use Bundle\CMS\Controller\ContentController;
+use Silex\Application;
 
 class CMSExtension extends SimpleExtension
 {
+    /** @var Application */
+    private $app;
+
+    public function boot(Application $app)
+    {
+        parent::boot($app);
+
+        $this->app = $app;
+    }
+
     protected function registerBackendControllers()
     {
         return [
             '/editcontent' => new ContentController(),
         ];
+    }
+
+    protected function registerTwigFunctions()
+    {
+        return [
+            'cities' => 'getCities',
+        ];
+    }
+
+    public function getCities()
+    {
+        $cities = $this->getContainer()['config']->get('taxonomy')['cities']['options'];
+        ksort($cities);
+
+        return $cities;
     }
 }
