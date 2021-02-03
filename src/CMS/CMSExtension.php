4dab6345306c322bcc0cc46c17274468d8e36e40
Ilya Shibailo <ilya.shibailo@gmail.com>
2021-02-04 02:26:51 +0700

    commit

--- src/CMS/CMSExtension.php
+++ src/CMS/CMSExtension.php
@@ -35,7 +35,8 @@ class CMSExtension extends SimpleExtension
     public function getCities()
     {
         $cities = $this->getContainer()['config']->get('taxonomy')['cities']['options'];
-        sort($cities);
+
+        asort($cities);
 
         return $cities;
     }
