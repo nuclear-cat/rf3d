9e382b6c7d872d625795fcaf332533ff0f05e04e
Ilya Shibailo <ilya.shibailo@gmail.com>
2021-03-10 23:13:10 +0700

    commit

--- public/theme/base-2018/src/place_page.js
+++ public/theme/base-2018/src/place_page.js
@@ -2,17 +2,15 @@ import $ from "jquery";
 
 $('.place-slider').slick({
     lazyLoad: 'progressive',
-    dots: true,
-    adaptiveHeight: true,
-    asNavFor: '.place-slider-nav'
+    dots: false,
+    adaptiveHeight: false,
+    asNavFor: '.place-slider-nav',
 });
 
 $('.place-slider-nav').slick({
     slidesToShow: 4,
-
     slidesToScroll: 1,
     asNavFor: '.place-slider',
     dots: false,
-    // centerMode: true,
     focusOnSelect: true
 });
